0dc8829d5a3591fa0c08bd174c5d2dfea6dae63e
leosaq <leandrosaquisari7@gmail.com>
2026-05-21 11:50:42 -0500

    cambios problema01

--- ejemplo02/consulta_datos_01.py
+++ ejemplo02/consulta_datos_01.py
@@ -21,18 +21,21 @@ session = Session()
 # Obtener todos los registros de
 # la entidad estudiante (clase Estudiante)
 
-# estudiantes = session.query(Estudiante).all()
-# print(estudiantes)
+#estudiantes = session.query(Estudiante).all()
+#print(estudiantes)
 
-# print("--------------------------------------")
+#print("--------------------------------------")
 # Obtener todos los registros de la clase Modulo
-# modulos = session.query(Modulo).all()
-# print(modulos)
+#modulos = session.query(Modulo).all()
+#print(modulos)
 
-# print("--------------------------------------")
+print("--------------------------------------")
 # Obtener todos los registros de la clase Matricula
-# matriculas = session.query(Matricula).all()
-
+matriculas = session.query(Matricula).all()
+for m in matriculas:
+    print(m, m.estudiante, m.modulo)
+    print(m.estudiante.nombre, m.estudiante.apellido)
+print("--------------------------------------")
 # nombre y apellido del estudiante de cada matrícula
 
 # print(matriculas)
